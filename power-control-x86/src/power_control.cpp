27a1cfdd31c72ed5ce0c7e952db0fbdd214dc5e9
nikitapbst <pavlov@bisyst.ru>
2021-07-19 18:39:12 +0300

    q

--- power-control-x86/src/power_control.cpp
+++ power-control-x86/src/power_control.cpp
@@ -1705,7 +1705,7 @@ static void powerStateTransitionToOff(const Event event)
         case Event::psPowerOKDeAssert:
             // Cancel any GPIO assertions held during the transition
             gpioAssertTimer.cancel();
-            setPowerState(PowerState::off);
+            setPowerState(PowerState::on);
             break;
         default:
             phosphor::logging::log<phosphor::logging::level::INFO>(
